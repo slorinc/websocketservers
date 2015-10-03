9cef8eaad68d6c7c2b2ef58af17fd5ac3ead9099
Lorinc Sonnevend <slorinc@gmail.com>
2015-10-03 12:34:40 +0200

    form validation

--- servera/src/main/java/com/comeon/servera/controllers/AdminController.java
+++ servera/src/main/java/com/comeon/servera/controllers/AdminController.java
@@ -3,11 +3,15 @@ package com.comeon.servera.controllers;
 import com.comeon.servera.beans.RefreshValuesBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.validation.Valid;
 
 /**
  * Created by s_lor_000 on 10/2/2015.
@@ -18,20 +22,25 @@ public class AdminController {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(AdminController.class);
 
-
     @Autowired
     private RefreshValuesBean refreshValuesBean;
 
+
     @RequestMapping
-    public String admin(@ModelAttribute RefreshValuesBean refreshValuesBean) {
+    public String admin(Model model) {
+        model.addAttribute("refreshValuesBean",refreshValuesBean);
         return "form";
     }
 
     @RequestMapping(value = "post", method = RequestMethod.POST)
-    public String handleFromPost(@ModelAttribute RefreshValuesBean refreshValuesBean) {
+    public String handleFromPost(@Valid @ModelAttribute RefreshValuesBean refreshValuesBean, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()){
+            return "form";
+        }
         LOGGER.info("{} values added through admin form.", refreshValuesBean.toString());
         this.refreshValuesBean.setRefreshData(refreshValuesBean.getRefreshData());
         this.refreshValuesBean.setRefreshView(refreshValuesBean.getRefreshView());
+        model.addAttribute("successMessage","Refresh times are updated successfully.");
         return "form";
     }
 }
